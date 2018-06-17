62552473713e47bb9e5db4b0365047face2f059b
KingVoodoo <cajunbypass@gmail.com>
2018-06-16 23:45:14 -0500

    Update seat.php

--- src/lang/en/seat.php
+++ src/lang/en/seat.php
@@ -10,20 +10,20 @@
 
 return [
 
-    'seat_groups'    => 'SeAT Groups',
-    'seat_groups_dashboard' => 'Dashboard',
+    'seat_groups'    => 'Careers',
+    'seat_groups_dashboard' => 'Portal',
 
-    'seat_groups_autogroup' => 'Auto SeAT Groups',
-    'seat_groups_autogroup_description' => 'To these SeAT Groups you are automatically assigned based on your corporation membership',
+    'seat_groups_autogroup' => 'Enlistment',
+    'seat_groups_autogroup_description' => 'If you have your ALOT in CheckBox.. you will be in this group',
 
-    'seat_groups_opengroup' => 'Open SeAT Groups',
-    'seat_groups_opengroup_description' => 'In these SeAT Groups you can freely opt-in and opt-out as you wish.',
+    'seat_groups_opengroup' => 'A.L.O.Ts',
+    'seat_groups_opengroup_description' => 'Assign your ALOT a squad here!',
 
-    'seat_groups_managedgroup' => 'Managed SeAT Groups',
-    'seat_groups_managedgroup_description' => 'Here you can apply for certain groups. The managers of this group will approve or deny your request.',
+    'seat_groups_managedgroup' => 'Omega Careers',
+    'seat_groups_managedgroup_description' => 'Apply here if you can help Goon Berets in one of the following roles.',
     'seat_groups_manager' => 'Manager',
-    'seat_join_opengroup'=>'Opt in',
-    'seat_leave_opengroup'=>'Opt out',
+    'seat_join_opengroup'=>'Enlist',
+    'seat_leave_opengroup'=>'Decommision',
     'seat_leave_waitlist' => 'Leave',
     'seat_join_waitlist' => 'Apply',
     'seat_leave_managedgroup' => 'Leave',
@@ -31,17 +31,18 @@ return [
     'seat_accept_managedgroup' => 'Accept',
     'seat_removefrom_managedgroup' => 'Remove',
 
-    'seat_groups_hiddengroup' => 'Hidden SeAT Group',
+    'seat_groups_hiddengroup' => 'SOCOM',
     'seat_groups_user' => 'User',
     'seat_groups_adduser' => 'Add User',
 
-    'seat_groups_create' => 'Create',
-    'seat_groups_create_new' => 'Create a new SeAT Group',
+    'seat_groups_create' => 'Create Companies',
+    'seat_groups_create_new' => 'Create a new Company',
     'seat_groups_name' => 'Name',
-    'seat_groups_description' => 'SeAT Group Description',
-    'seat_groups_type' => 'Select SeAT Group Type',
-    'seat_groups_role' => 'Select corresponding SeAT Role',
-    'seat_groups_add' => 'Add new SeAT Group',
+    'seat_groups_description' => 'Description/Skill Plan',
+    'seat_groups_skillplan' => 'Skill Plan',
+    'seat_groups_type' => 'Company Type',
+    'seat_groups_role' => 'MOS',
+    'seat_groups_add' => 'Create MOS/Company',
 
     'seat_groups_edit' => 'Add new SeAT Group',
     'seat_groups_editing' => 'Editing',
